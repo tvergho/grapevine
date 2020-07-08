13de3ca32c8bb30e7ff39da76c11fe349c3ddc8f
tvergho <tvergho@gmail.com>
2020-07-08 15:08:58 -0700

    Deleete friend

--- screens/Friends/FriendsScreen.js
+++ screens/Friends/FriendsScreen.js
@@ -5,8 +5,11 @@ import {
 } from 'react-native';
 import ModalHeader from 'components/ModalHeader';
 import FriendsItem from 'components/FriendsItem';
-import { getFriendRequests, getFriends } from 'actions';
+import { getFriendRequests, getFriends, deleteFriend } from 'actions';
 import { connect } from 'react-redux';
+import Swipeable from 'react-native-swipeable-row';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faUserTimes } from '@fortawesome/free-solid-svg-icons';
 import FriendRequestsCard from './FriendRequestsCard';
 
 const FriendsScreen = (props) => {
@@ -20,13 +23,26 @@ const FriendsScreen = (props) => {
   const { requestsLoading, friendsLoading } = props.friends;
   const loading = requestsLoading || friendsLoading;
 
+  const leftContent = (
+    <View style={{
+      backgroundColor: '#EB6660', flex: 1, alignItems: 'flex-end', justifyContent: 'center',
+    }}
+    >
+      <FontAwesomeIcon icon={faUserTimes} size={40} color="white" style={{ marginRight: 15 }} />
+    </View>
+  );
+
   return (
     <View style={styles.background}>
       <ModalHeader navigation={navigation} title="Friends" />
       {!loading ? <FriendRequestsCard requests={props.friends.requests} navigation={navigation} /> : <></>}
       <FlatList
         data={loading ? Array.from(Array(5).keys()) : props.friends.friends}
-        renderItem={({ item }) => (<FriendsItem loading={loading} user={item} type="send" />)}
+        renderItem={({ item }) => (
+          <Swipeable leftContent={leftContent} onLeftActionRelease={() => { props.deleteFriend(item.UserID); }}>
+            <FriendsItem loading={loading} user={item} type="send" />
+          </Swipeable>
+        )}
         keyExtractor={(friend) => friend.username}
       />
     </View>
@@ -46,4 +62,4 @@ const mapStateToProps = (reduxState) => (
   }
 );
 
-export default connect(mapStateToProps, { getFriendRequests, getFriends })(FriendsScreen);
+export default connect(mapStateToProps, { getFriendRequests, getFriends, deleteFriend })(FriendsScreen);
