376b79370e130df6b791f40517bac82121b37b52
tvergho <tvergho@gmail.com>
2020-09-16 08:54:29 -0700

    Fixed profile pic bug

--- utils/getFileData.js
+++ utils/getFileData.js
@@ -1,6 +1,6 @@
 const getFileData = (uri) => {
   const re = /(?:\.([^.]+))?$/;
-  const extension = re.exec('file.name.with.dots.txt')[1];
+  const extension = re.exec(uri)[1];
   const fileType = extension === 'jpg' || extension === 'jpeg' ? 'image/jpeg' : 'image/png';
 
   return { extension, fileType };
